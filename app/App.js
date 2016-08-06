95111f50ecc7a985e77de1ef41ecbe2ba286522f
bencallis1 <bencallis1@gmail.com>
2016-08-06 10:07:45 -0600

    changes with node_modules

--- app/App.js
+++ app/App.js
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, hashHistory  } from 'react-router';
 import routes from './config/routes';
-import Home from './containers/Home/Home';
-
 
 // first argument is the component that we need to render
 // second argument is where to render the component
-
 ReactDOM.render(
     <Router history={hashHistory}>{routes}</Router>,
     document.getElementById('app')
